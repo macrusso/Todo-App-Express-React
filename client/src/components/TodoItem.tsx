3f856480a18efda1bc7ce5e4a844bfb312746939
Mac <mac.rusek@nokkel.com>
2024-09-09 17:09:01 +0100

    ADD: handle complete uncomplete todo

--- client/src/components/TodoItem.tsx
+++ client/src/components/TodoItem.tsx
@@ -2,13 +2,13 @@ import { Todo } from '../types/ToDo';
 
 interface TodoItemProps {
   todo: Todo;
-  toggleTodo: (id: number) => void;
+  toggleTodo: (id: string, isCompleted: boolean) => void;
 }
 
 const TodoItem = ({ todo, toggleTodo }: TodoItemProps) => {
   return (
     <li
-      onClick={() => toggleTodo(todo.id)}
+      onClick={() => toggleTodo(todo.id, todo.isCompleted)}
       className={`cursor-pointer p-4 my-2 border rounded ${
         todo.isCompleted ? 'bg-green-200 line-through' : 'bg-gray-100'
       }`}
--- client/src/components/TodoList.tsx
+++ client/src/components/TodoList.tsx
@@ -1,32 +1,82 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import TodoItem from './TodoItem';
 import { Todo } from '../types/ToDo';
 
-const TodoList = () => {
-  const [todos, setTodos] = useState<Todo[]>([
-    { id: 1, content: 'Learn TypeScript', isCompleted: false },
-    { id: 2, content: 'Setup Tailwind CSS', isCompleted: false },
-    { id: 3, content: 'Build a Todo App', isCompleted: false },
-  ]);
+const USER_ID = '9790a4f1-115a-4ba7-88e5-b07c31ed7c6b';
+const TODO_ENDPOINT = 'http://localhost:3001/todos';
 
-  // Toggle the completed state of a todo item
-  const toggleTodo = (id: number) => {
-    setTodos((prevTodos) =>
-      prevTodos.map((todo) =>
-        todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
-      )
-    );
+const TodoList = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await fetch(`${TODO_ENDPOINT}/${USER_ID}`);
+
+      if (!response.ok) {
+        throw new Error();
+      }
+
+      const data: Todo[] = await response.json();
+      setTodos(data);
+      setLoading(false);
+    } catch (err) {
+      setError('Something went wrong');
+      setLoading(false);
+    }
+  };
+
+  const updateTodoStatus = async (todoId: string, isCompleted: boolean) => {
+    try {
+      const response = await fetch(`${TODO_ENDPOINT}/${todoId}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ isCompleted: !isCompleted }),
+      });
+
+      if (!response.ok) {
+        throw new Error();
+      }
+
+      const updatedTodo = await response.json();
+
+      setTodos((prevTodos) =>
+        prevTodos.map((todo) =>
+          todo.id === updatedTodo.id ? updatedTodo : todo
+        )
+      );
+    } catch (err) {
+      setError('Something went wrong');
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const toggleTodo = (id: string, isCompleted: boolean) => {
+    updateTodoStatus(id, isCompleted);
   };
 
   return (
-    <div className='max-w-md mx-auto p-6 bg-white shadow-lg rounded-lg'>
-      <h1 className='text-2xl font-bold mb-4'>Todo List</h1>
-      <ul>
-        {todos.map((todo) => (
-          <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} />
-        ))}
-      </ul>
-    </div>
+    <>
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {!loading && !error && (
+        <div className='w-[500px] mx-auto p-6 bg-white shadow-lg rounded-lg'>
+          <h1 className='text-2xl font-bold mb-4'>Todo List</h1>
+          <ul>
+            {todos.map((todo) => (
+              <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} />
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
   );
 };
 
